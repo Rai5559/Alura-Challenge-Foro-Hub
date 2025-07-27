83cc2a72a27d6cb219877adde25c3d4a45ff5e4d
Rai5559 <icexbee1@gmail.com>
2025-07-26 21:11:07 -0600

    Add EnvLoader class to manage environment variables and update .gitignore

--- src/main/java/com/rai69/Foro_Hub/ForoHubApplication.java
+++ src/main/java/com/rai69/Foro_Hub/ForoHubApplication.java
@@ -3,11 +3,15 @@ package com.rai69.Foro_Hub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rai69.Foro_Hub.util.EnvLoader;
+
 @SpringBootApplication
 public class ForoHubApplication {
 
 	public static void main(String[] args) {
+		EnvLoader envLoader = new EnvLoader();
 		SpringApplication.run(ForoHubApplication.class, args);
+		System.out.println("Foro Hub Application has started successfully!");
 	}
 
 }
