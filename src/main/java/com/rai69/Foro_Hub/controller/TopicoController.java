da38df6008f0dab3e1e580b70e55d39710f0c7cc
Rai5559 <icexbee1@gmail.com>
2025-07-27 21:24:04 -0600

    Enhance TopicoController to support default pagination parameters; update application.properties for clarity

--- src/main/java/com/rai69/Foro_Hub/controller/TopicoController.java
+++ src/main/java/com/rai69/Foro_Hub/controller/TopicoController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -52,7 +54,9 @@ public class TopicoController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<TopicoResponseDTO>> listarTopicos(Pageable pageable) {
+    public ResponseEntity<Page<TopicoResponseDTO>> listarTopicos(
+            @PageableDefault(size = 10, page = 0, sort = "fechaCreacion", direction = Sort.Direction.ASC) 
+            Pageable pageable) {
         try {
             Page<TopicoResponseDTO> response = topicoService.listarTopicos(pageable);
             return ResponseEntity.ok(response);
